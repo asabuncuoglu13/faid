aef04b9436da481581d65e51be76f268fcbdd4be
Alpay Sabuncuoglu <asabuncuoglu13@ku.edu.tr>
2024-12-26 00:12:13 +0300

    log-sync

--- faid/logging/conversion.py
+++ faid/logging/conversion.py
@@ -1,45 +1,101 @@
-def sync_risk_to_model():
-    # Implement the logic to sync risk to model
+from faid.logging import (
+    get_model_log_file_path,
+    get_risk_register_log_path,
+    get_model_entry,
+    add_model_entry,
+    add_data_entry,
+    get_data_entry,
+    get_risk_entry,
+    add_risk_entry,
+)
+
+model_file_path = get_model_log_file_path()
+risk_file_path = get_risk_register_log_path()
+
+def sync_model_to_risk():
+    model_risks = get_model_entry("considerations").get('risks', [])
+    
+    # Sync the related entities
+    for i, risk in enumerate(model_risks):
+       add_risk_entry({
+            'description': risk.get('name', ''),
+            'impact': '',  # Add logic to determine impact if needed
+            'likelihood': '',  # Add logic to determine likelihood if needed
+            'mitigation': risk.get('mitigation_strategy', '')
+        }, key='risks')
+
+def sync_model_to_data():
+    model_data = get_model_entry("data")
+    add_data_entry({
+        "summary": model_data.get('description', ''),
+        "dataset_link": model_data.get('link', ''),
+        "sensitivity_types": model_data.get('sensitive', ''),
+    })
+
+def sync_model_to_usecase():
+    # Implement the logic to sync model to usecase
     pass
 
 def sync_data_to_model():
-    # Implement the logic to sync data to model
+    data_entry = {
+        "description" : get_data_entry("summary"),
+        "link" : get_data_entry("dataset_link"),
+        "sensitive" : get_data_entry("sensitivity_types"),
+        "graphics" : "",
+    }
+    add_model_entry(data_entry, key='data', filename=model_file_path)
+
+def sync_data_to_risk():
+    data_entry = get_data_entry(key='risks')
+
+    for entry in data_entry:
+        dr = {
+            'description': entry.get('name', ''),
+            'impact': '',  # Add logic to determine impact if needed
+            'likelihood': '',  # Add logic to determine likelihood if needed
+            'mitigation': entry.get('mitigation_strategy', '')
+        }
+        add_risk_entry(dr, key='risks')
+
+def sync_data_to_transparency():
+    # Implement the logic to sync data to transparency
+    pass
+
+def sync_risk_to_model():
+    risk_entry = get_risk_entry(key='risks')
+    model_risks = risk_entry.get('risks', [])
+    
+    # Sync the related entities
+    for i, risk in enumerate(model_risks):
+        add_model_entry({
+            'name': risk.get('description', ''),
+            'mitigation_strategy': risk.get('mitigation', '')
+        }, key='considerations', filename=model_file_path)
+
+def sync_risk_to_data():
+    risk_entry = get_risk_entry(key='risks')
+    data_risks = risk_entry.get('risks', [])
+
+    for i, risk in enumerate(data_risks):
+        add_data_entry({
+            'name': risk.get('description', ''),
+            'mitigation_strategy': risk.get('mitigation', '')
+        }, key='risks')
+
+def sync_risk_to_transparency():
+    # Implement the logic to sync risk to transparency
+    pass
+
+def sync_model_to_transparency():
+    # Implement the logic to sync model to transparency
     pass
 
 def sync_usecase_to_model():
     # Implement the logic to sync usecase to model
     pass
 
-def sync_model_to_risk():
-    # Implement the logic to sync model to risk
-    pass
-
-def sync_model_to_data():
-    # Implement the logic to sync model to data
-    pass
-
-def sync_model_to_usecase():
-    # Implement the logic to sync model to usecase
-    pass
-
-def sync_risk_to_data():
-    # Implement the logic to sync risk to data
-    pass
-
 def sync_usecase_to_data():
     # Implement the logic to sync usecase to data
-    pass
-
-def sync_risk_to_transparency():
-    # Implement the logic to sync risk to transparency
-    pass
-
-def sync_data_to_transparency():
-    # Implement the logic to sync data to transparency
-    pass
-
-def sync_model_to_transparency():
-    # Implement the logic to sync model to transparency
     pass
 
 def sync_usecase_to_transparency():
